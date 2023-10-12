9b357fc0e4e42258dfdbb45a646f61578bacdd79
Vilas Shetkar <vilasshetkar@gmail.com>
2023-10-12 12:53:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mcqQuestionRoutes = require('./routes/mcqQuestionRoutes');
-const mcqResultRoutes = require('./routes/mcqResultRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mcqQuestionRoutes = require("./routes/mcqQuestionRoutes");
+const mcqResultRoutes = require("./routes/mcqResultRoutes");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,24 +12,28 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Vilas Shetkar's MCQ Api");
+});
+
 // Routes
-app.use('/api', mcqQuestionRoutes);
-app.use('/api', mcqResultRoutes);
+app.use("/api", mcqQuestionRoutes);
+app.use("/api", mcqResultRoutes);
 
 // MongoDB Connection
 mongoose
   .connect(
-    'mongodb+srv://ngmcq:8lB3Qm5j6DYpWn3z@cluster1.2lk9mic.mongodb.net/ngmcq?retryWrites=true&w=majority',
+    "mongodb+srv://ngmcq:8lB3Qm5j6DYpWn3z@cluster1.2lk9mic.mongodb.net/ngmcq?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    }
+    },
   )
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
+    console.error("Error connecting to MongoDB:", err);
   });
 
 // Start the server
